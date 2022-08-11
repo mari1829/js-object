07377c3eb302cc2976c33b48d467077ce7fabfd7
mari <sermarajraj35@gmail.com>
2022-08-11 03:56:48 -0800

    object-basic

---
+++ script.js
@@ -0,0 +1,144 @@
+let studentsdetails = {
+  firstname: "Mari",
+  lastname: "Selvam",
+  Age: "24",
+  qualification: "B.E",
+};
+
+console.log(
+  "My Name Is ",
+  studentsdetails.firstname,
+  studentsdetails.lastname,
+  "My Age Is",
+  studentsdetails.Age,
+  "My Qualification Is",
+  studentsdetails.qualification
+);
+
+let mobiles = {
+  brand: "Redmi",
+  Model: "Note 7Pro",
+  ram: "4GB",
+  camera: "48px"
+};
+
+console.log("Brand is",mobiles.brand,"Model is",mobiles.Model,"Ram is",mobiles.ram,"camera is",mobiles.camera);
+
+let headset={
+    brand:"boat",
+    type:"neck band",
+    batterylife:"70hrs"
+}
+console.log("Headset is",headset.brand,"Type is",headset.type,"Battery life is ",headset.batterylife);
+
+let laptop={
+    brand:"Lenovo",
+    os:"windows 11",
+    frontcamera:"10px"
+
+}
+console.log(laptop.brand,laptop.os,laptop.frontcamera);
+
+let bike={
+    brand:"Yamaha",
+    model:"RX-100",
+    color:"Black with orange"
+}
+
+console.log(bike.brand,bike.model,bike.color);
+
+let car={
+    brand:"hundai",
+    model:"i20",
+    color:"red"
+}
+console.log(car.brand,car.model,car.color);
+
+let shirt={
+    brand:"otto",
+    size:"L-size",
+    color:"white"
+}
+console.log( shirt.brand, shirt.size, shirt.color);
+
+let tshirt={
+    brand:"Nike",
+    size:"M",
+    color:"red"
+}
+console.log(tshirt.brand,tshirt.size,tshirt.color);
+
+let tv={
+    brand:"sony",
+    size:"43inc",
+    type:"LED"
+    
+}
+console.log(tv.brand,tv.size,tv.type);
+
+let tyre={
+    brand:"MRF",
+    size:"Twowheeler",
+    type:"tubeless"
+}
+console.log( tyre.brand, tyre.size, tyre.type);
+
+let mouse={
+    brand:"Hp",
+    type:"wireless",
+    batterylife:"1000hrs"
+}
+console.log(mouse.brand,mouse.type,mouse.batterylife);
+
+let keyboard={
+    brand:"logi",
+    type:"wireless",
+    batterylife:"1000hrs"
+    
+}
+console.log(keyboard.brand,keyboard.type,keyboard.batterylife);
+
+let fridge={
+    brand:"Samsung",
+    capacity:"190 ltr",
+    color:"red",
+
+}
+console.log(fridge.brand,fridge.capacity,fridge.color);
+
+let fan={
+    company:"Bajaj",
+    rpm:"1500",
+    color:"brown"
+}
+console.log(fan.company,fan.rpm,fan.color);
+
+let bulb={
+    company:"phlips",
+    type:"LED",
+    waranty:"2Years"
+}
+
+console.log(bulb.company,bulb.type,bulb.waranty);
+
+
+let cooldrinks={
+    brand:"sprite",
+    Qty:"2 ltr",
+    price:"70rs"
+}
+console.log(cooldrinks.brand,cooldrinks.Qty,cooldrinks.price);
+
+let milk={
+    brand:"avin",
+    qty:"1 Ltr",
+    amount:"50 rs"
+}
+console.log(milk.brand,milk.qty,milk.qty);
+
+let tractor={
+    brand:"mahendra",
+    model:"Maharaja",
+    color:"Yello and Green"
+}
+console.log(tractor.brand,tractor.model,tractor.color);
